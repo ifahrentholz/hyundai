c6867c871d5f24f54b06cef0b9a75bccb033934a
Ingo Fahrentholz <ingo.fahrentholz@diva-e.com>
2024-07-03 05:38:58 +0200

    add video-teaser text

--- src/blocks/teaser-video/teaser-video.ts
+++ src/blocks/teaser-video/teaser-video.ts
@@ -1,5 +1,5 @@
 import { html, render } from 'lit-html';
-// import { unsafeHTML } from 'lit/directives/unsafe-html.js';
+import { unsafeHTML } from 'lit/directives/unsafe-html.js';
 
 import './teaser-video.scss';
 
@@ -14,29 +14,34 @@ type Data = {
 const renderVideo = (videoUrl: string | undefined) => {
   if (!videoUrl) return null;
   return html`
-<video autoplay muted loop playsinline class="w-full max-h-screen object-cover">
+<video autoplay="true" muted="true" loop="true" playsinline="true" class="w-full h-full object-cover absolute left-0 top-0 bottom-0 right-0">
   <source src="${videoUrl}"></source>
 </video>
 `;
 };
 
-// const renderHeadline = (headline: string | undefined) => {
-//   if (!headline) return null;
-//   return html`<h2 class="relative z-10 text-white font-hyundaihead text-[48px] font-medium m-0 mb-[8px]">
-//     ${headline}
-//   </h2>`;
-// };
+const renderHeadline = (headline: string | undefined) => {
+  if (!headline) return null;
+  return html`<h2 class="relative z-10 text-white font-hyundaihead text-[48px] font-medium m-0 mb-[8px]">
+    ${headline}
+  </h2>`;
+};
 
-// const renderSubheadline = (subheadline: string | undefined) => {
-//   if (!subheadline) return null;
-//   return html`<h3 class="relative z-10 text-white font-hyundaitext text-[20px] m-0 font-normal mb-[40px]">
-//     ${subheadline}
-//   </h3>`;
-// };
+const renderSubheadline = (subheadline: string | undefined) => {
+  if (!subheadline) return null;
+  return html`<h3 class="relative z-10 text-white font-hyundaitext text-[20px] m-0 font-normal mb-[40px]">
+    ${subheadline}
+  </h2>`;
+};
 
 const template = (data: Data) => {
   return html`
-    <div id="teaser-video" class="text-center bg-hyundai-blue overflow-hidden relative">${renderVideo(data.video)}</div>
+    <div id="teaser-video" class="flex flex-col justify-end text-center bg-hyundai-blue overflow-hidden relative">
+      ${renderVideo(data.video)}
+      <div class="absolute left-6 top-10">${unsafeHTML(data.logo)}</div>
+      <div class="absolute right-6 top-10">${unsafeHTML(data.award)}</div>
+      <div>${renderHeadline(data.headline)} ${renderSubheadline(data.subheadline)}</div>
+    </div>
   `;
 };
 
